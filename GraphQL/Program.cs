04bafd48bad1cbda10af724fb010ada9d9b2dc1c
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-05-04 17:04:18 +0800

    added root query

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,3 +1,4 @@
+using ConferencePlanner.GraphQL;
 using ConferencePlanner.GraphQL.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,8 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
 
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// Configure the HTTP request pipeline.
+
+
+// app.UseHttpsRedirection();
+
+
+// app.MapControllers();
+app.UseWebSockets();
+
+app.UseRouting();
+// app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+  endpoints.MapGraphQL();
+});
 
 app.Run();
