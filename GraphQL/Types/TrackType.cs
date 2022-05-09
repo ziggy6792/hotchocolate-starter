11cdfc009c0037083aca78303a135f75f518e75b
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-05-09 17:53:52 +0800

    session 5

--- GraphQL/Types/TrackType.cs
+++ GraphQL/Types/TrackType.cs
@@ -11,39 +11,44 @@ using HotChocolate.Types;
 
 namespace ConferencePlanner.GraphQL.Types
 {
-    public class TrackType : ObjectType<Track>
+  public class TrackType : ObjectType<Track>
+  {
+    protected override void Configure(IObjectTypeDescriptor<Track> descriptor)
     {
-        protected override void Configure(IObjectTypeDescriptor<Track> descriptor)
-        {
-            descriptor
-                .ImplementsNode()
-                .IdField(t => t.Id)
-                .ResolveNode((ctx, id) =>
-                    ctx.DataLoader<TrackByIdDataLoader>().LoadAsync(id, ctx.RequestAborted));
+      descriptor
+          .ImplementsNode()
+          .IdField(t => t.Id)
+          .ResolveNode((ctx, id) =>
+              ctx.DataLoader<TrackByIdDataLoader>().LoadAsync(id, ctx.RequestAborted));
 
-            descriptor
-                .Field(t => t.Sessions)
-                .ResolveWith<TrackResolvers>(t => t.GetSessionsAsync(default!, default!, default!, default))
-                .UseDbContext<ApplicationDbContext>()
-                .UsePaging<NonNullType<SessionType>>()
-                .Name("sessions");
-        }
+      descriptor
+          .Field(t => t.Sessions)
+          .ResolveWith<TrackResolvers>(t => t.GetSessionsAsync(default!, default!, default!, default))
+          .UseDbContext<ApplicationDbContext>()
+          .UsePaging<NonNullType<SessionType>>()
+          .Name("sessions");
 
-        private class TrackResolvers
-        {
-            public async Task<IEnumerable<Session>> GetSessionsAsync(
-                Track track,
-                [ScopedService] ApplicationDbContext dbContext,
-                SessionByIdDataLoader sessionById,
-                CancellationToken cancellationToken)
-            {
-                int[] sessionIds = await dbContext.Sessions
-                    .Where(s => s.Id == track.Id)
-                    .Select(s => s.Id)
-                    .ToArrayAsync();
+      descriptor
+        .Field(t => t.Name)
+        .UseUpperCase();
 
-                return await sessionById.LoadAsync(sessionIds, cancellationToken);
-            }
-        }
     }
+
+    private class TrackResolvers
+    {
+      public async Task<IEnumerable<Session>> GetSessionsAsync(
+          Track track,
+          [ScopedService] ApplicationDbContext dbContext,
+          SessionByIdDataLoader sessionById,
+          CancellationToken cancellationToken)
+      {
+        int[] sessionIds = await dbContext.Sessions
+            .Where(s => s.Id == track.Id)
+            .Select(s => s.Id)
+            .ToArrayAsync();
+
+        return await sessionById.LoadAsync(sessionIds, cancellationToken);
+      }
+    }
+  }
 }
