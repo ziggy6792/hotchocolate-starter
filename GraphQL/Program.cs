39be8fff5079a32e0770b97ec845dc66cbe8e563
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-05-12 17:43:37 +0800

    auth resolver

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using ConferencePlanner.GraphQL;
 using ConferencePlanner.GraphQL.Attendees;
 using ConferencePlanner.GraphQL.Data;
@@ -6,49 +7,72 @@ using ConferencePlanner.GraphQL.Sessions;
 using ConferencePlanner.GraphQL.Speakers;
 using ConferencePlanner.GraphQL.Tracks;
 using ConferencePlanner.GraphQL.Types;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
 // builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
-builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=conferences.db"));
+
+
+var signingKey = new SymmetricSecurityKey(
+    Encoding.UTF8.GetBytes("MySuperSecretKey"));
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidIssuer = "https://auth.chillicream.com",
+                ValidAudience = "https://graphql.chillicream.com",
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey
+            };
+    });
 
 builder.Services
     .AddGraphQLServer()
+    .AddAuthorization()
     .AddQueryType<Query>()
-        .AddTypeExtension<SpeakerQueries>()
-        .AddTypeExtension<SessionQueries>()
-        .AddTypeExtension<TrackQueries>()
+    .AddTypeExtension<SpeakerQueries>()
+    .AddTypeExtension<SessionQueries>()
+    .AddTypeExtension<TrackQueries>()
     .AddMutationType<Mutation>()
-       .AddTypeExtension<AttendeeMutations>()
-        .AddTypeExtension<SpeakerMutations>()
-        .AddTypeExtension<SessionMutations>()
-        .AddTypeExtension<TrackMutations>()
-         .AddType<AttendeeType>()
+    .AddTypeExtension<AttendeeMutations>()
+    .AddTypeExtension<SpeakerMutations>()
+    .AddTypeExtension<SessionMutations>()
+    .AddTypeExtension<TrackMutations>()
+    .AddType<AttendeeType>()
     .AddSubscriptionType<Subscription>()
-     .AddTypeExtension<AttendeeSubscriptions>()
-     .AddTypeExtension<SessionSubscriptions>()
+    .AddTypeExtension<AttendeeSubscriptions>()
+    .AddTypeExtension<SessionSubscriptions>()
     .AddType<SessionType>()
     .AddType<TrackType>()
     .AddType<SpeakerType>()
-     .AddGlobalObjectIdentification()
-     .AddQueryFieldToMutationPayloads()
+    .AddGlobalObjectIdentification()
+    .AddQueryFieldToMutationPayloads()
     .AddDataLoader<SpeakerByIdDataLoader>()
     .AddDataLoader<SessionByIdDataLoader>()
     .AddFiltering()
-   .AddSorting()
-   .AddInMemorySubscriptions();
+    .AddSorting()
+    .AddInMemorySubscriptions();
 
 
 var app = builder.Build();
 
+
 app.UseWebSockets();
 app.UseRouting();
 
-app.UseEndpoints(endpoints =>
-{
-  endpoints.MapGraphQL();
-});
+app.UseAuthentication();
+
+
+app.UseEndpoints(endpoints => { endpoints.MapGraphQL(); });
 
 app.Run();
--- GraphQL/Speakers/SpeakerQueries.cs
+++ GraphQL/Speakers/SpeakerQueries.cs
@@ -1,34 +1,37 @@
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using ConferencePlanner.GraphQL.Data;
 using ConferencePlanner.GraphQL.DataLoader;
 using HotChocolate;
+using HotChocolate.AspNetCore.Authorization;
 using HotChocolate.Types;
 using HotChocolate.Types.Relay;
 
-namespace ConferencePlanner.GraphQL.Speakers
+namespace ConferencePlanner.GraphQL.Speakers;
+
+[ExtendObjectType(typeof(Query))]
+public class SpeakerQueries
 {
-  [ExtendObjectType(typeof(Query))]
-  public class SpeakerQueries
-  {
     [UseApplicationDbContext]
     [UsePaging]
+    [Authorize]
     public IQueryable<Speaker> GetSpeakers(
-        [ScopedService] ApplicationDbContext context) =>
-        context.Speakers.OrderBy(t => t.Name);
+        [ScopedService] ApplicationDbContext context)
+    {
+        return context.Speakers.OrderBy(t => t.Name);
+    }
 
     public Task<Speaker> GetSpeakerByIdAsync(
         [ID(nameof(Speaker))] int id,
         SpeakerByIdDataLoader dataLoader,
-        CancellationToken cancellationToken) =>
-        dataLoader.LoadAsync(id, cancellationToken);
+        CancellationToken cancellationToken)
+    {
+        return dataLoader.LoadAsync(id, cancellationToken);
+    }
 
     public async Task<IEnumerable<Speaker>> GetSpeakersByIdAsync(
         [ID(nameof(Speaker))] int[] ids,
         SpeakerByIdDataLoader dataLoader,
-        CancellationToken cancellationToken) =>
-        await dataLoader.LoadAsync(ids, cancellationToken);
-  }
+        CancellationToken cancellationToken)
+    {
+        return await dataLoader.LoadAsync(ids, cancellationToken);
+    }
 }
